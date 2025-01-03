b5ac1635e8b6babddf2faf92d6cde7ed4d817f6c
Oisín <opeppard@cmcl.io>
2025-01-03 21:13:35 +0000

    new eslint configuration format

--- code/eslint.config.js
+++ code/eslint.config.js
@@ -1,40 +1,47 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'next'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    plugins: [
-        '@typescript-eslint',
-        'react'
-    ],
-    rules: {
-        'no-unused-vars': 'off',
-        'no-extra-semi': 'warn',
-        'no-console': [
-            'warn',
-            {
-                allow: ['info', 'warn', 'error']
-            }
-        ],
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            {
-                argsIgnorePattern: '^_',
-                varsIgnorePattern: '^_',
-                caughtErrorsIgnorePattern: '^_'
-            }
-        ]
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginNext from "@next/eslint-plugin-next"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'], // Add this if you are using React 17+
+  {
+    settings: {
+      react: {
+        version: "detect",
+        // ...
+      },
     }
-};
+  },
+  {
+    plugins: {
+      '@next/next': pluginNext
+    },
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: "warn"
+    }
+  },
+  {
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  }
+];
