aedff467d6a67cc168c2a45cbcdbe71cd9c0b91e
Oisín <opeppard@cmcl.io>
2025-01-21 17:42:10 +0000

    add current year to footer

--- code/src/ui/text/footer.tsx
+++ code/src/ui/text/footer.tsx
@@ -8,11 +8,15 @@ import { Assets } from 'io/config/assets';
  * Renders a footer.
  */
 export default function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
       <Image alt={"TWA Logo"} src={Assets.TWA} width={30} height={30} style={{ paddingRight: 5 }} />
       <span>Powered by&nbsp;
         <Link href="https://theworldavatar.io">The World Avatar</Link>
+        &nbsp;{currentYear}
       </span>
     </footer >
   );
