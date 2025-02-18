79277ab4ba7816889aba29975fcdd72083c4b9e4
Kok Foong Lee <kfl31@cam.ac.uk>
2025-02-18 11:20:56 +0800

    dev-parse-timeseries: parse time series data from FIA correctly, removed TimeSeriesGroup, added chartjs-adapter-moment to display times correctly

--- code/src/ui/graphic/table/table.tsx
+++ code/src/ui/graphic/table/table.tsx
@@ -1,33 +1,33 @@
 import styles from './table.module.css';
 
 
-import { TimeSeries, TimeSeriesGroup } from 'types/timeseries';
+import { TimeSeries } from 'types/timeseries';
 
 interface TableProps {
-  group: TimeSeriesGroup;
-  selectedIndex: number;
+    data: TimeSeries[];
+    selectedIndex: number;
 }
 
 /**
  * This component renders a table populated with the time series data.
  * 
- * @param {TimeSeriesGroup} group The group to render.
+ * @param {TimeSeries[]} data The group to render.
  * @param {number} selectedIndex The currently selected index.
  */
 export default function Table(props: Readonly<TableProps>) {
-  const currentTimeSeries: TimeSeries = props.group.data[props.selectedIndex];
-  return (
-    <div className={styles["table"]}>
-      <p>Data series</p>
-      <div className={styles["content"]}>
-        {currentTimeSeries.values.map((value, index) => (
-          <div key={index} className={styles["entry"]}>
-            <span className={styles["entry-header"]}>{value}</span>
-            <span>at date/time:</span>
-            <span>{props.group.times[index]}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const currentTimeSeries: TimeSeries = props.data[props.selectedIndex];
+    return (
+        <div className={styles["table"]}>
+            <p>Data series</p>
+            <div className={styles["content"]}>
+                {currentTimeSeries.values.map((value, index) => (
+                    <div key={index} className={styles["entry"]}>
+                        <span className={styles["entry-header"]}>{value}</span>
+                        <span>at date/time:</span>
+                        <span>{currentTimeSeries.times[index]}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
--- code/src/ui/interaction/tree/info/time-series-panel.tsx
+++ code/src/ui/interaction/tree/info/time-series-panel.tsx
@@ -2,38 +2,38 @@ import styles from './info-tree.module.css';
 
 import { useState } from 'react';
 
-import { TimeSeriesGroup } from 'types/timeseries';
+import { TimeSeries } from 'types/timeseries';
 import Chart from 'ui/graphic/chart/chart';
 import Table from 'ui/graphic/table/table';
 import DropdownField, { DropdownFieldOption } from 'ui/interaction/dropdown/dropdown';
 
 interface TimeSeriesPanelProps {
-  data: TimeSeriesGroup;
+    data: TimeSeries[];
 }
 
 /**
  * This component is responsible for displaying time series information as a panel.
  * 
- * @param {TimeSeriesGroup} data The queried time series data.
+ * @param {TimeSeries[]} data The queried time series data.
  */
 export default function TimeSeriesPanel(props: Readonly<TimeSeriesPanelProps>) {
-  const tsData: TimeSeriesGroup = props.data;
-  const [selectedTimeSeriesOption, setSelectedTimeSeriesOption] = useState(0);
+    const tsData: TimeSeries[] = props.data;
+    const [selectedTimeSeriesOption, setSelectedTimeSeriesOption] = useState(0);
 
-  const parseTimeSeriesIntoOptions = (timeSeries: TimeSeriesGroup): DropdownFieldOption[] => {
-    const options: DropdownFieldOption[] = [];
-    timeSeries.data.map((timeSeries, index) => {
-      const label: string = timeSeries.unit === "-" ? timeSeries.name : timeSeries.name + " [" + timeSeries.unit + "]";
-      options.push({ index: index, label: label });
-    });
-    return options;
-  };
+    const parseTimeSeriesIntoOptions = (timeSeries: TimeSeries[]): DropdownFieldOption[] => {
+        const options: DropdownFieldOption[] = [];
+        timeSeries.forEach((ts, index) => {
+            const label: string = ts.unit === "-" ? ts.name : ts.name + " [" + ts.unit + "]";
+            options.push({ index: index, label: label });
+        });
+        return options;
+    };
 
-  return (
-    <div className={styles["time-series-panel"]}>
-      <DropdownField options={parseTimeSeriesIntoOptions(tsData)} selectedIndex={selectedTimeSeriesOption} setSelectedIndex={setSelectedTimeSeriesOption} />
-      <Chart data={tsData} selectedIndex={selectedTimeSeriesOption} />
-      <Table group={tsData} selectedIndex={selectedTimeSeriesOption} />
-    </div>
-  );
+    return (
+        <div className={styles["time-series-panel"]}>
+            <DropdownField options={parseTimeSeriesIntoOptions(tsData)} selectedIndex={selectedTimeSeriesOption} setSelectedIndex={setSelectedTimeSeriesOption} />
+            <Chart data={tsData} selectedIndex={selectedTimeSeriesOption} />
+            <Table data={tsData} selectedIndex={selectedTimeSeriesOption} />
+        </div>
+    );
 }
