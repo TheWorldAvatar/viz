14a165e7fdf995a056a47d5b9fa26bbfb2781644
Kok Foong Lee <kfl31@cam.ac.uk>
2025-02-21 13:12:21 +0800

    dev-parse-timeseries: whitespace change only

--- code/src/ui/graphic/table/table.tsx
+++ code/src/ui/graphic/table/table.tsx
@@ -4,8 +4,8 @@ import styles from './table.module.css';
 import { TimeSeries } from 'types/timeseries';
 
 interface TableProps {
-    data: TimeSeries[];
-    selectedIndex: number;
+  data: TimeSeries[];
+  selectedIndex: number;
 }
 
 /**
@@ -15,19 +15,19 @@ interface TableProps {
  * @param {number} selectedIndex The currently selected index.
  */
 export default function Table(props: Readonly<TableProps>) {
-    const currentTimeSeries: TimeSeries = props.data[props.selectedIndex];
-    return (
-        <div className={styles["table"]}>
-            <p>Data series</p>
-            <div className={styles["content"]}>
-                {currentTimeSeries.values.map((value, index) => (
-                    <div key={index} className={styles["entry"]}>
-                        <span className={styles["entry-header"]}>{value}</span>
-                        <span>at date/time:</span>
-                        <span>{currentTimeSeries.times[index]}</span>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const currentTimeSeries: TimeSeries = props.data[props.selectedIndex];
+  return (
+    <div className={styles["table"]}>
+      <p>Data series</p>
+      <div className={styles["content"]}>
+        {currentTimeSeries.values.map((value, index) => (
+          <div key={index} className={styles["entry"]}>
+            <span className={styles["entry-header"]}>{value}</span>
+            <span>at date/time:</span>
+            <span>{currentTimeSeries.times[index]}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
