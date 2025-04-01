2af668afa88bac3d12945d2f6d3a6e943bc4067d
Oisín <opeppard@cmcl.io>
2025-04-01 18:28:06 +0100

    use the Dictionary type in the getdictionary method used for static props

--- code/src/app/registry/[lang]/dictionaries.ts
+++ code/src/app/registry/[lang]/dictionaries.ts
@@ -1,12 +1,13 @@
-import 'server-only'
+import 'server-only';
+import { Dictionary } from 'types/dictionary';
 
-const dictionaries = {
+const dictionaries: Record<string, () => Promise<Dictionary>> = {
     'de': () => import('dictionaries/de.json').then((module) => module.default),
     'en-GB': () => import('dictionaries/en-GB.json').then((module) => module.default),
-}
+};
 
-export const getDictionary = async (locale: 'en-GB' | 'de') =>
+export const getDictionary = async (locale: 'en-GB' | 'de'): Promise<Dictionary> =>
     dictionaries[locale]().catch((error) => {
         console.error(`Failed to load dictionary for locale: ${locale}`, error);
         throw error;
-    })
+    });
