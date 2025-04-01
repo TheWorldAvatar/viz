730f09e42bd129314fd262d9ac58000044588407
Oisín <opeppard@cmcl.io>
2025-04-01 18:27:10 +0100

    add a dictionary context provider for reg pages

---
+++ code/src/app/registry/[lang]/DictionaryContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from 'react';
+import { Dictionary } from 'types/dictionary';
+
+const DictionaryContext = createContext<Dictionary | null>(null);
+
+export const DictionaryProvider = ({
+    children,
+    dictionary,
+}: {
+    children: React.ReactNode;
+    dictionary: Dictionary;
+}) => {
+    return (
+        <DictionaryContext.Provider value={dictionary}>
+            {children}
+        </DictionaryContext.Provider>
+    );
+};
+
+export const useDictionary = () => {
+    const context = useContext(DictionaryContext);
+    if (!context) {
+        throw new Error('useDictionary must be used within a DictionaryProvider');
+    }
+    return context;
+};
