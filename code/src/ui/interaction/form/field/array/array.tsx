ceae149f201aa7cf8dfbd7ce9724b39f6d928e0b
qhouyee <quekhouyee@gmail.com>
2025-04-01 17:21:45 +0100

    qhouyee/139/form-array-design: update array design

--- code/src/ui/interaction/form/field/array/array.tsx
+++ code/src/ui/interaction/form/field/array/array.tsx
@@ -1,13 +1,11 @@
 import styles from './array.module.css';
 
-import React, { useEffect } from 'react';
-import { useFieldArray, UseFormReturn, useWatch } from 'react-hook-form';
+import React, { useState } from 'react';
+import { useFieldArray, UseFormReturn } from 'react-hook-form';
 
-import { useBackgroundImageUrl } from 'hooks/useBackgroundImageUrl';
 import { PropertyShape } from 'types/form';
 import ClickActionButton from 'ui/interaction/action/click/click-button';
 import { DependentFormSection } from 'ui/interaction/form/section/dependent-form-section';
-import { isValidIRI } from 'utils/client-utils';
 import FormFieldComponent from '../form-field';
 
 export interface FormArrayProps {
@@ -29,85 +27,74 @@ export interface FormArrayProps {
  * @param {boolean} options.disabled Optional indicator if the field should be disabled. Defaults to false.
  */
 export default function FormArray(props: Readonly<FormArrayProps>) {
-  const { control } = props.form;
-  const backgroundImageUrl: string = useBackgroundImageUrl();
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
+  const { control } = props.form;
   const { fields, append, remove } = useFieldArray({
     control,
     name: props.fieldId,
   });
-  const fieldSize: number = fields.length - 1;
-  // Retrieve the live view of the last element in the array
-  const lastRow: Record<string, string> = useWatch({
-    control,
-    name: `${props.fieldId}.${fieldSize}`,
-  });
 
-  useEffect(() => {
-    const newRow: Record<string, object> = {};
-    // If a user adds an input into the last row
-    // Append a new row
-    let allNullOrEmpty: boolean = true;
-    for (const key in lastRow) {
-      if (key != "id" && (lastRow[key] && lastRow[key] != "" && !isValidIRI(lastRow[key]))) {
-        allNullOrEmpty = false;
-        break;
-      }
-      newRow[key] = null;
-    }
-    if (!allNullOrEmpty) {
-      append(newRow);
-    }
-  }, [lastRow])
+  const newRow: Record<string, object> = React.useMemo(() => Object.fromEntries(
+    Object.entries(fields[fields.length - 1]).map(([key, value]) => [key, { value }])), []);
 
   return (
-    <ul className={styles["row-container"]}>
-      {fields.map((field, index) => {
-        return (
-          <li key={field.id} className={styles["row"]}
-            style={{
-              backgroundImage: `url(${backgroundImageUrl})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}>
-            <span className={styles["row-header"]}>
-              <p className={styles["row-text"]}>
-                <span className={styles["row-marker"]}>{index + 1}</span>
-                {index == fieldSize && "Fill in the details to create a new entry"}
-              </p>
-              {index < fieldSize && <ClickActionButton
-                icon={"remove"}
-                className={`${styles["delete-button"]} ${styles["delete-button-background"]}`}
-                onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
-                  event.preventDefault();
-                  remove(index);
-                }}
-              />}
-            </span>
-            {props.fieldConfigs.map((config, secondaryIndex) => {
-              return <div key={field.id + index + secondaryIndex} className={styles["cell"]}>
-                {config.class && <DependentFormSection
-                  agentApi={props.agentApi}
-                  dependentProp={{
-                    ...config,
-                    fieldId: `${props.fieldId}.${index}.${config.fieldId}`,
-                  }}
-                  form={props.form}
-                />}
-                {!config.class && <FormFieldComponent
-                  agentApi={props.agentApi}
-                  field={{
-                    ...config,
-                    fieldId: `${props.fieldId}.${index}.${config.fieldId}`,
-                  }}
-                  form={props.form}
-                  options={props.options}
-                />}
-              </div>
-            })}
-          </li>
-        );
-      })}
-    </ul>
+    <div className={styles["container"]}>
+      <div className={styles["tab-container"]}>
+        <ClickActionButton
+          icon={"add"}
+          className={`${styles["row-marker"]} ${styles["add-button-background"]}`}
+          isTransparent={true}
+          onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+            event.preventDefault();
+            append(newRow);
+          }}
+        />
+        {fields.length > 1 && <ClickActionButton
+          icon={"remove"}
+          className={`${styles["delete-button"]} ${styles["delete-button-background"]}`}
+          onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+            event.preventDefault();
+            remove(currentIndex);
+          }}
+        />}
+        {Array.from({ length: fields.length }, (_, index) => (
+          <ClickActionButton
+            key={index}
+            icon={index === currentIndex ? "expand_more" : "chevron_right"}
+            className={styles["row-marker"]}
+            isActive={index === currentIndex}
+            isTransparent={true}
+            onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+              event.preventDefault();
+              setCurrentIndex(index);
+            }}
+          />
+        ))}
+      </div>
+      <div className={styles["row"]}>
+        {props.fieldConfigs.map((config, secondaryIndex) => {
+          return <div key={currentIndex + secondaryIndex} className={styles["cell"]}>
+            {config.class && <DependentFormSection
+              agentApi={props.agentApi}
+              dependentProp={{
+                ...config,
+                fieldId: `${props.fieldId}.${currentIndex}.${config.fieldId}`,
+              }}
+              form={props.form}
+            />}
+            {!config.class && <FormFieldComponent
+              agentApi={props.agentApi}
+              field={{
+                ...config,
+                fieldId: `${props.fieldId}.${currentIndex}.${config.fieldId}`,
+              }}
+              form={props.form}
+              options={props.options}
+            />}
+          </div>
+        })}
+      </div>
+    </div>
   );
 }
