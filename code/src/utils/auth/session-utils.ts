59efe007662ed70a7911bacf173f0bc89214d7a4
Oisín <opeppard@cmcl.io>
2025-04-11 16:28:12 +0100

    updated object initialisation in session-utils.ts

--- code/src/utils/auth/session-utils.ts
+++ code/src/utils/auth/session-utils.ts
@@ -3,20 +3,24 @@
  */
 
 import { Routes } from "io/config/routes";
-import { PermissionScheme } from "types/auth";
+import { HasPermissions, PermissionScheme } from "types/auth";
+
+const hasPermitionsInitial: HasPermissions = {
+    pendingRegistry: false,
+    activeArchiveRegistry: false,
+    invoice: false,
+    sales: false,
+    operation: false,
+    viewTask: false,
+    completeTask: false,
+    reportTask: false,
+    export: false,
+};
 
 export function parsePermissions(roles: string[]): PermissionScheme {
     const permissionScheme: PermissionScheme = {
-        route: null,
-        pendingRegistry: false,
-        activeArchiveRegistry: false,
-        export: false,
-        invoice: false,
-        sales: false,
-        operation: false,
-        viewTask: false,
-        completeTask: false,
-        reportTask: false,
+        registryPageLink: null,
+        hasPermissions: hasPermitionsInitial
     }
     if (roles.length === 0) {
         return permissionScheme;
@@ -24,41 +28,41 @@ export function parsePermissions(roles: string[]): PermissionScheme {
 
     // Access to different registry depends on roles
     if (roles.includes("registry-navigation")) {
-        permissionScheme.pendingRegistry = true;
-        permissionScheme.activeArchiveRegistry = true;
+        permissionScheme.hasPermissions.pendingRegistry = true;
+        permissionScheme.hasPermissions.activeArchiveRegistry = true;
     } else if (roles.includes("registry-navigation-pending")) {
         // Users with only access to the pending registry cannot access other registries
-        permissionScheme.route = Routes.REGISTRY_PENDING;
+        permissionScheme.registryPageLink = Routes.REGISTRY_PENDING;
     } else if (roles.includes("registry-navigation-active-archive")) {
         // Users with only access to the active and archive registry cannot access the pending registry
         // and will be redirected
-        permissionScheme.route = Routes.REGISTRY_ACTIVE;
-        permissionScheme.activeArchiveRegistry = true;
+        permissionScheme.registryPageLink = Routes.REGISTRY_ACTIVE;
+        permissionScheme.hasPermissions.activeArchiveRegistry = true;
     }
 
     // Roles with access to only specific routes
     if (roles.includes("task-viewer")) {
-        permissionScheme.route = Routes.REGISTRY_TASK_DATE;
-        permissionScheme.completeTask = true;
-        permissionScheme.reportTask = true;
+        permissionScheme.registryPageLink = Routes.REGISTRY_TASK_DATE;
+        permissionScheme.hasPermissions.completeTask = true;
+        permissionScheme.hasPermissions.reportTask = true;
     }
     if (roles.includes("finance")) {
-        permissionScheme.route = Routes.REGISTRY_ACTIVE;
-        permissionScheme.invoice = true;
+        permissionScheme.registryPageLink = Routes.REGISTRY_ACTIVE;
+        permissionScheme.hasPermissions.invoice = true;
     }
 
     // General permissions
     if (roles.includes("sales")) {
-        permissionScheme.sales = true;
+        permissionScheme.hasPermissions.sales = true;
     }
     if (roles.includes("operations")) {
-        permissionScheme.operation = true;
-        permissionScheme.viewTask = true;
-        permissionScheme.completeTask = true;
-        permissionScheme.reportTask = true;
+        permissionScheme.hasPermissions.operation = true;
+        permissionScheme.hasPermissions.viewTask = true;
+        permissionScheme.hasPermissions.completeTask = true;
+        permissionScheme.hasPermissions.reportTask = true;
     }
     if (roles.includes("export")) {
-        permissionScheme.export = true;
+        permissionScheme.hasPermissions.export = true;
     }
     return permissionScheme
 }
