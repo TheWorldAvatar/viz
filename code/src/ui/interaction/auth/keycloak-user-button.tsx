17c9b9db7fad74dff72d1546b8947744798a923b
PDochev <plamendochev0@gmail.com>
2025-05-23 15:49:39 +0100

    style: removed the tooltip from the usermenu, improved the style and added a border between the content

--- code/src/ui/interaction/auth/keycloak-user-button.tsx
+++ code/src/ui/interaction/auth/keycloak-user-button.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import Link from 'next/link';
-import styles from './keycloak-user-button.module.css';
-import { useUserDisplayName } from 'hooks/auth/useUserDisplayName';
-import PopoverActionButton from '../action/popover/popover-button';
-
+import Link from "next/link";
+import styles from "./keycloak-user-button.module.css";
+import { useUserDisplayName } from "hooks/auth/useUserDisplayName";
+import PopoverActionButton from "../action/popover/popover-button";
 
 /**
  * This component renders a widget that displays the user and a log out button.
-*
-*/
+ *
+ */
 export default function KeycloakUserButton() {
-        const userDisplayName = useUserDisplayName();
-        const userInitial: string = userDisplayName.charAt(0).toUpperCase();
+  const userDisplayName = useUserDisplayName();
+  const userInitial: string = userDisplayName.charAt(0).toUpperCase();
 
-    return (
-
-        <PopoverActionButton
-            label={userInitial}
-            tooltipText="User"
-            tooltipPosition="bottom"
-            isHoverableDisabled={true}
-            isTransparent={true}
-            styling={{ text: styles.text}}
-            placement="bottom-end"
-            className={styles.userMenuButton}
-            >
-                <div className={styles.userMenuContainer}>
-                            <span>{userDisplayName}</span>
-                   <div className={styles.seperationLine}></div>
-                      <Link prefetch={false} href="/logout">Log Out</Link>
-                </div>
-             </PopoverActionButton>
-    );
-};
+  return (
+    <PopoverActionButton
+      label={userInitial}
+      isHoverableDisabled={true}
+      isTransparent={true}
+      styling={{ text: styles.text }}
+      placement="bottom-end"
+      className={styles.userMenuButton}
+    >
+      <div className={styles.userMenuContainer}>
+        <div className={styles.userButtons}>
+          <span>{userDisplayName}</span>
+        </div>
+        <div className={styles.userButtons}>
+          <Link prefetch={false} href="/logout">
+            Log Out
+          </Link>
+        </div>
+      </div>
+    </PopoverActionButton>
+  );
+}
