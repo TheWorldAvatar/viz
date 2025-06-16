e445545b36083c3a36f6dc54487a7d8ade952dbc
plamendochev <pdochev@cmcl.io>
2025-06-16 11:12:26 +0100

    Improving the button component

--- code/src/ui/interaction/button.tsx
+++ code/src/ui/interaction/button.tsx
@@ -1,4 +1,7 @@
 import React, { ButtonHTMLAttributes } from "react";
+import { Placement } from "@floating-ui/react";
+import { Icon } from "@mui/material";
+import Tooltip from "ui/interaction/tooltip/tooltip";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?:
@@ -12,11 +15,30 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     | "ghost";
   size?: "sm" | "md" | "lg" | "default" | "icon";
   spinnerSize?: "sm" | "md" | "lg" | "icon";
-  leftIcon?: React.ReactNode;
-  rightIcon?: React.ReactNode;
-  loading?: boolean; // Optional loading state
-  onClick?: () => void; // Optional click handler
+  leftIcon?: "string" | React.ReactNode;
+  rightIcon?: "string" | React.ReactNode;
+  loading?: boolean;
+  label?: string;
+  tooltipText?: string;
+  tooltipPosition?: Placement;
+  disabled?: boolean;
 }
+
+/**
+ * A generic  button template class.
+ *
+ * @param {string} variant The button variant, e.g., "primary", "secondary", etc. This controls the button's appearance.
+ * @param {string} size The button size, e.g., "sm", "md", "lg", "default", or "icon". This controls the button's dimensions and padding.
+ * @param {string} spinnerSize The size of the loading spinner, e.g.,
+ * @param {string} leftIcon Optional left icon, can be a string or React node.
+ * @param {string} rightIcon Optional right icon, can be a string or React
+ * @param {boolean} loading Optional loading state to show a spinner.
+ * @param {string} label Optional label for the button.
+ * @param {string} tooltipText Optional label that is displayed as a tooltip on hover.
+ * @param {Placement} tooltipPosition Optional tooltip position.
+ * @param {boolean} disabled Optional disabled state for the button.
+ *
+ */
 
 // LoadingSpinner component to show a spinner when the button is in loading state
 const LoadingSpinner = ({
@@ -59,101 +81,97 @@ const LoadingSpinner = ({
   );
 };
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      className, // Allow custom classes to be passed in
-      variant = "primary", // Default variant
-      size = "md", // Default size
-      spinnerSize = "md", // Default spinner size
-      onClick,
-      leftIcon,
-      rightIcon,
-      children,
-      disabled,
-      loading = false, // Default loading state to false
-      ...props
-    },
-    ref
-  ) => {
-    // Base styles for the button, applied to all variants and sizes
-    const baseStyles =
-      "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive";
+export default function Button({
+  className, // Allow custom classes to be passed in
+  variant = "primary", // Default variant
+  size = "md", // Default size
+  spinnerSize = "md", // Default spinner size
+  onClick,
+  leftIcon,
+  rightIcon,
+  children,
+  disabled,
+  loading = false, // Default loading state to false
+  label,
+  tooltipText,
+  tooltipPosition = "top", // Default tooltip position
+  ...props
+}: Readonly<ButtonProps>) {
+  // Base styles for the button, applied to all variants and sizes
+  const baseStyles =
+    "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive";
 
-    // Define styles for each variant
-    const variantStyles = {
-      primary: "bg-primary text-primary-foreground hover:bg-primary/90",
-      link: "text-primary-foreground underline-offset-4 hover:underline",
-      success:
-        "bg-blue-300 hover:bg-blue-500/80 dark:bg-blue-800 dark:hover:bg-blue-900/80",
-      warning:
-        "bg-amber-300 hover:bg-amber-500/80 dark:bg-amber-800 dark:hover:bg-amber-900/80",
-      secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300",
-      danger:
-        "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-      outline:
-        "bg-transparent border border-border text-gray-700 hover:bg-gray-100",
-      ghost: "bg-transparent text-gray-700 hover:bg-gray-100",
-    };
+  // Define styles for each variant
+  const variantStyles = {
+    primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+    link: "text-primary-foreground underline-offset-4 hover:underline",
+    success:
+      "bg-blue-300 hover:bg-blue-500/80 dark:bg-blue-800 dark:hover:bg-blue-900/80",
+    warning:
+      "bg-amber-300 hover:bg-amber-500/80 dark:bg-amber-800 dark:hover:bg-amber-900/80",
+    secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300",
+    danger:
+      "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+    outline:
+      "bg-transparent border border-border text-gray-700 hover:bg-gray-100",
+    ghost: "bg-transparent text-gray-700 hover:bg-gray-100",
+  };
 
-    // Define styles for each size
-    const sizeStyles = {
-      sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-      md: "px-4 py-2 text-base",
-      default: "h-9 px-4 py-2 has-[>svg]:px-3",
-      lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-      icon: "size-9",
-    };
+  // Define styles for each size
+  const sizeStyles = {
+    sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+    md: "px-4 py-2 text-base",
+    default: "h-9 px-4 py-2 has-[>svg]:px-3",
+    lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+    icon: "size-9",
+  };
 
-    // Define spacing between icons based on size
-    const iconSpacing = {
-      sm: "space-x-1",
-      md: "space-x-1.5",
-      default: "space-x-2",
-      lg: "space-x-2",
-      icon: "space-x-0",
-    };
+  // Define spacing between icons based on size
+  const iconSpacing = {
+    sm: "space-x-1",
+    md: "space-x-1.5",
+    default: "space-x-2",
+    lg: "space-x-2",
+    icon: "space-x-0",
+  };
 
-    // Define styles for the disabled state
-    const disabledStyles = "opacity-50 cursor-not-allowed";
-    // Define styles for the loading state
-    const loadingStyles = "opacity-75 cursor-wait";
+  // Define styles for the disabled state
+  const disabledAndLoadingStyles = "opacity-50 cursor-not-allowed";
 
-    // Build the className string
-    const buttonClasses = [
-      baseStyles,
-      variantStyles[variant],
-      sizeStyles[size],
-      disabled ? disabledStyles : "",
-      loading ? loadingStyles : "",
-      className, // This allows for additional custom classes to be passed in
-    ]
-      .filter(Boolean) // Remove any empty strings (e.g., from the disabled conditional)
-      .join(" "); // Join them into a single string
+  // Build the className string
+  const buttonClasses = [
+    baseStyles,
+    variantStyles[variant],
+    sizeStyles[size],
+    disabled || loading ? disabledAndLoadingStyles : "",
+    className, // This allows for additional custom classes to be passed in
+  ]
+    .filter(Boolean) // Remove any empty strings (e.g., from the disabled conditional)
+    .join(" "); // Join them into a single string
 
-    return (
+  return (
+    <Tooltip text={tooltipText} placement={tooltipPosition}>
       <button
-        ref={ref}
         className={buttonClasses}
-        disabled={disabled}
+        disabled={disabled || loading}
         onClick={!disabled && !loading ? onClick : undefined}
         {...props}
       >
         <div className={`flex items-center ${iconSpacing[size]}`}>
           {loading && <LoadingSpinner size={spinnerSize} />}
           {!loading && leftIcon && (
-            <span className="flex items-center">{leftIcon}</span>
+            <span className="flex items-center">
+              {<Icon className="material-symbols-outlined">{leftIcon}</Icon>}
+            </span>
           )}
-          <span>{children}</span>
+          <span>{children || label}</span>
           {!loading && rightIcon && (
-            <span className="flex items-center">{rightIcon}</span>
+            <span className="flex items-center">
+              {<Icon className="material-symbols-outlined">{rightIcon}</Icon>}
+            </span>
           )}
         </div>
       </button>
-    );
-  }
-);
-
-Button.displayName = "Button"; // Set display name for debugging
-
-export default Button;
+    </Tooltip>
+  );
+}
