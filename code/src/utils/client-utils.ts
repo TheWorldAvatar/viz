4e70a100153a053630a57974e20b6ab048ef62cf
plamendochev <pdochev@cmcl.io>
2025-07-02 14:25:21 +0100

    Fixed the issue where the characters like ä were wrongly displayed.

--- code/src/utils/client-utils.ts
+++ code/src/utils/client-utils.ts
@@ -1,12 +1,18 @@
 /**
  * Utilities to be run on the client.
  */
-import { Dispatch } from 'redux';
+import { Dispatch } from "redux";
 
-import { DataParser } from 'io/data/data-parser';
-import { DataStore } from 'io/data/data-store';
-import { MapFeaturePayload, clearFeatures, setIri, setProperties, setStack } from 'state/map-feature-slice';
-import { RegistryFieldValues, SparqlResponseField } from 'types/form';
+import { DataParser } from "io/data/data-parser";
+import { DataStore } from "io/data/data-store";
+import {
+  MapFeaturePayload,
+  clearFeatures,
+  setIri,
+  setProperties,
+  setStack,
+} from "state/map-feature-slice";
+import { RegistryFieldValues, SparqlResponseField } from "types/form";
 import { JsonObject } from "types/json";
 
 /**
@@ -14,30 +20,33 @@ import { JsonObject } from "types/json";
  */
 
 export function openFullscreen() {
-    const elem = document?.documentElement;
-    if (elem?.requestFullscreen) {
-        elem.requestFullscreen();
-    }
+  const elem = document?.documentElement;
+  if (elem?.requestFullscreen) {
+    elem.requestFullscreen();
+  }
 }
 
 /**
  * Close fullscreen mode.
  */
 export function closeFullscreen() {
-    if (document?.exitFullscreen) {
-        document.exitFullscreen();
-    }
+  if (document?.exitFullscreen) {
+    document.exitFullscreen();
+  }
 }
 
 /**
  * Parses the contents of the data.json file into class instances for setting the map.
- * 
+ *
  * @param dataSettings Map data settings.
  * @param mapType The type of map. Either Cesium or Mapbox
  * @returns The data model required for visualisation.
  */
-export function parseMapDataSettings(dataSettings: JsonObject, mapType: string): DataStore {
-    return new DataParser(mapType).loadData(dataSettings);
+export function parseMapDataSettings(
+  dataSettings: JsonObject,
+  mapType: string
+): DataStore {
+  return new DataParser(mapType).loadData(dataSettings);
 }
 
 /**
@@ -46,76 +55,86 @@ export function parseMapDataSettings(dataSettings: JsonObject, mapType: string):
  * @param {MapFeaturePayload} selectedFeature The feature of interest.
  * @param {Dispatch<any>} dispatch The dispatch function from Redux for dispatching actions.
  */
-export function setSelectedFeature(selectedFeature: MapFeaturePayload, dispatch: Dispatch): void {
-    if (selectedFeature) {
-        // Disable linting as we wish to remove layer but do not require it in this function
-        const { _layer, stack, iri, ...selectedProperties } = selectedFeature;
-        if (!iri) {
-            console.warn("IRI is missing. Data fetching will be skipped.");
-        } else if (!stack) {
-            console.warn("Feature does not have a defined stack. Data fetching will be skipped.");
-        }
-        dispatch(setIri(iri));
-        dispatch(setProperties(selectedProperties));
-        dispatch(setStack(stack));
-        dispatch(clearFeatures());
+export function setSelectedFeature(
+  selectedFeature: MapFeaturePayload,
+  dispatch: Dispatch
+): void {
+  if (selectedFeature) {
+    // Disable linting as we wish to remove layer but do not require it in this function
+    const { _layer, stack, iri, ...selectedProperties } = selectedFeature;
+    if (!iri) {
+      console.warn("IRI is missing. Data fetching will be skipped.");
+    } else if (!stack) {
+      console.warn(
+        "Feature does not have a defined stack. Data fetching will be skipped."
+      );
     }
+    dispatch(setIri(iri));
+    dispatch(setProperties(selectedProperties));
+    dispatch(setStack(stack));
+    dispatch(clearFeatures());
+  }
 }
 
 /**
  * Capitalises the words.
- * 
+ *
  * @param {string} str input string.
  */
 export function parseWordsForLabels(str: string): string {
-    if (isValidIRI(str)) {
-        return getAfterDelimiter(str, "/");
-    }
-    return str.toLowerCase()
-        .replaceAll("_", " ")
-        .replace(/([a-z])([A-Z])/g, '$1 $2')
-        .split(" ")
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(" ");
+  // Decode the string first to handle URL-encoded characters, like ä and ß
+  const decodedStr: string = decodeURIComponent(str);
+
+  if (isValidIRI(decodedStr)) {
+    return getAfterDelimiter(decodedStr, "/");
+  }
+  return decodedStr
+    .toLowerCase()
+    .replaceAll("_", " ")
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
 /**
  * Replaces all white spaces with _ to ensure it is valid for urls
- * 
+ *
  * @param {string} str input string.
  */
 export function parseStringsForUrls(str: string): string {
-    return str.trim().replace(/\s+/g, "_");
+  return str.trim().replace(/\s+/g, "_");
 }
 
 /**
  * Retrieves the ID from input.
- * 
+ *
  * @param {string} input input string.
  */
 export function getId(input: string): string {
-    return isValidIRI(input) ? getAfterDelimiter(input, "/") : input;
+  return isValidIRI(input) ? getAfterDelimiter(input, "/") : input;
 }
 
 /**
  * Checks that the input iri is valid.
- * 
+ *
  * @param {string} iri input iri.
  */
 export function isValidIRI(iri: string): boolean {
-    // eslint-disable-next-line
-    const iriPattern = /^(https?|ftp|mailto|file|data|irc|tel|urn|uuid|doi):((\/\/[^\/?#]*)?[^?#]*)(\?[^#]*)?(#.*)?$/i;
-    return iriPattern.test(iri);
+  // eslint-disable-next-line
+  const iriPattern =
+    /^(https?|ftp|mailto|file|data|irc|tel|urn|uuid|doi):((\/\/[^\/?#]*)?[^?#]*)(\?[^#]*)?(#.*)?$/i;
+  return iriPattern.test(iri);
 }
 
 /**
  * Retrieves the string following the delimiter if it exists. Otherwise returns the string as is.
- * 
+ *
  * @param {string} str input string.
  * @param {string} delimiter delimiter of interest.
  */
 export function getAfterDelimiter(str: string, delimiter: string): string {
-    return str.includes(delimiter) ? str.split(delimiter).pop() : str;
+  return str.includes(delimiter) ? str.split(delimiter).pop() : str;
 }
 
 /**
@@ -124,7 +143,7 @@ export function getAfterDelimiter(str: string, delimiter: string): string {
  * @param {SparqlResponseField} response The target SPARQL response.
  */
 export function getSparqlResponseValue(response: SparqlResponseField): string {
-    return response.value;
+  return response.value;
 }
 
 /**
@@ -134,15 +153,21 @@ export function getSparqlResponseValue(response: SparqlResponseField): string {
  * @param {string} field The target field of interest.
  * @param {boolean} getFirstArrayField Optional indicator to retrieve the first array field if required.
  */
-export function extractResponseField(response: RegistryFieldValues, field: string, getFirstArrayField?: boolean): SparqlResponseField {
-    if (Array.isArray(response[field])) {
-        if (getFirstArrayField) {
-            return response[field][0];
-        } else {
-            console.warn(`Detected that field ${field} is an array! Skipping field...`)
-            return null;
-        }
+export function extractResponseField(
+  response: RegistryFieldValues,
+  field: string,
+  getFirstArrayField?: boolean
+): SparqlResponseField {
+  if (Array.isArray(response[field])) {
+    if (getFirstArrayField) {
+      return response[field][0];
     } else {
-        return response[field];
+      console.warn(
+        `Detected that field ${field} is an array! Skipping field...`
+      );
+      return null;
     }
+  } else {
+    return response[field];
+  }
 }
