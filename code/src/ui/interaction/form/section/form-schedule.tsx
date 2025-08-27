03996f8745c70309321ce1cb1b44ab4de32cca82
plamendochev <pdochev@cmcl.io>
2025-08-27 12:08:44 +0100

    Set default time slot values in FormSchedule component (from 00:00 to 23:59)

--- code/src/ui/interaction/form/section/form-schedule.tsx
+++ code/src/ui/interaction/form/section/form-schedule.tsx
@@ -153,6 +153,8 @@ export default function FormSchedule(props: Readonly<FormScheduleProps>) {
     if (formType == "add" || formType == "search") {
       props.form.setValue(FORM_STATES.RECURRENCE, 0);
       setSelectedServiceOption(singleService);
+      props.form.setValue(FORM_STATES.TIME_SLOT_START, "00:00");
+      props.form.setValue(FORM_STATES.TIME_SLOT_END, "23:59");
       setIsLoading(false);
     } else {
       getAndSetScheduleDefaults();
@@ -276,7 +278,7 @@ export default function FormSchedule(props: Readonly<FormScheduleProps>) {
               </div>
             </div>
           )}
-          <div className="w-full mt-8 ">
+          <div className="w-full mt-8">
             <h1 className="text-xl font-bold mb-2">{dict.form.timeSlot}</h1>
             <FormFieldComponent
               field={{
