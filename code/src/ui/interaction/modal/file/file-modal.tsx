afaed40780ea3429007a46f0d2e3e32de25d5868
qhouyee <quekhouyee@gmail.com>
2025-09-03 15:51:06 +0000

    moved placement to bottom

--- code/src/ui/interaction/modal/file/file-modal.tsx
+++ code/src/ui/interaction/modal/file/file-modal.tsx
@@ -105,6 +105,7 @@ export default function FileModal(props: Readonly<FileModalProps>) {
           {props.type === "date" && <DateRangeInput
             selectedDate={selectedDate}
             setSelectedDate={setSelectedDate}
+            placement="bottom"
           />}
         </section>
 
