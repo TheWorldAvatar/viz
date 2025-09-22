a2f0acef4d16fb47f58352f6814d41736f7d10e4
qhouyee <quekhouyee@gmail.com>
2025-09-22 15:03:59 +0000

    fix missing status due to different casing

--- code/src/ui/text/status/status.tsx
+++ code/src/ui/text/status/status.tsx
@@ -100,7 +100,7 @@ export default function StatusComponent(props: Readonly<StatusComponentProps>) {
           backgroundColor: statusBackgroundColor,
         }}
       >
-        {parseWordsForLabels(dict.title[props.status] ?? props.status)}
+        {parseWordsForLabels(dict.title[props.status.toLowerCase()] ?? props.status)}
       </p>
     </span>
   );
