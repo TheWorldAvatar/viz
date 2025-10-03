406df2e837153891f4da1284cc8bd52aabfe7d5c
plamendochev <pdochev@cmcl.io>
2025-10-03 11:06:47 +0100

    Replaced the old CSS module styles with  Tailwindcss classes. Removed legend-tree module css file

--- code/src/ui/interaction/tree/legend/legend-tree.tsx
+++ code/src/ui/interaction/tree/legend/legend-tree.tsx
@@ -1,12 +1,11 @@
-import parentStyles from '../floating-panel.module.css';
-import styles from './legend-tree.module.css';
+import parentStyles from "../floating-panel.module.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import { LegendGroup, LegendSettings } from 'types/settings';
-import DecagonIconComponent from 'ui/graphic/icon/decagon';
-import IconComponent from 'ui/graphic/icon/icon';
-import HeaderField from 'ui/text/header';
+import { LegendGroup, LegendSettings } from "types/settings";
+import DecagonIconComponent from "ui/graphic/icon/decagon";
+import IconComponent from "ui/graphic/icon/icon";
+import HeaderField from "ui/text/header";
 
 // Incoming parameters for component.
 interface LegendTreeProps {
@@ -23,9 +22,11 @@ interface LegendTreeNodeProps {
  */
 export default function LegendTree(props: Readonly<LegendTreeProps>) {
   return (
-    <div className={styles.legendContainer}>
+    <div className="w-full h-[inherit] flex flex-col p-4">
       {Object.entries(props.settings).map(([groupName, group]) => {
-        return <LegendTreeNode key={groupName} groupName={groupName} group={group} />
+        return (
+          <LegendTreeNode key={groupName} groupName={groupName} group={group} />
+        );
       })}
     </div>
   );
@@ -33,13 +34,18 @@ export default function LegendTree(props: Readonly<LegendTreeProps>) {
 
 function LegendTreeNode(props: Readonly<LegendTreeNodeProps>) {
   const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
-  const collapsedIcon: string = isCollapsed ? "keyboard_arrow_right" : "keyboard_arrow_down";
+  const collapsedIcon: string = isCollapsed
+    ? "keyboard_arrow_right"
+    : "keyboard_arrow_down";
   const toggleExpansion = () => {
     setIsCollapsed(!isCollapsed);
   };
 
   return (
-    <div key={props.groupName} className={styles.legendGroup}>
+    <div
+      key={props.groupName}
+      className="w-full block flex-col mb-1 box-border p-1.5 hover:bg-background-secondary"
+    >
       <HeaderField
         name={props.groupName}
         icon={collapsedIcon}
@@ -50,30 +56,28 @@ function LegendTreeNode(props: Readonly<LegendTreeNodeProps>) {
         toggleExpansion={toggleExpansion}
       />
       {Object.entries(props.group).map(([item, legendSettings]) => {
-          if (!isCollapsed) {
-            return (
-              <div key={props.groupName + item}>
-                <div className={styles.legendEntryHeader}>
-                  {legendSettings.type === "symbol" &&
-                    <IconComponent
-                      icon={legendSettings.icon}
-                    />}
-                  {legendSettings.type === "fill" &&
-                    <DecagonIconComponent
-                      color={legendSettings.fill}
-                    />}
-                  {legendSettings.description ? (
-                    <h4 className={styles.header}>{item}</h4>
-                  ) : (
-                    <span className={styles.legendEntryDescription}>{item}</span>
-                  )}
-                </div>
-                {legendSettings.description && (
-                  <span className={styles.legendEntryDescription}>{legendSettings.description}</span>
+        if (!isCollapsed) {
+          return (
+            <div key={props.groupName + item}>
+              <div className="grid grid-cols-[1.6rem_calc(100%-1.6rem)] gap-2 mt-2 items-center">
+                {legendSettings.type === "symbol" && (
+                  <IconComponent icon={legendSettings.icon} />
+                )}
+                {legendSettings.type === "fill" && (
+                  <DecagonIconComponent color={legendSettings.fill} />
+                )}
+                {legendSettings.description ? (
+                  <h4 className="text-base">{item}</h4>
+                ) : (
+                  <span className="text-sm">{item}</span>
                 )}
               </div>
-            );
-          }
+              {legendSettings.description && (
+                <span className="text-sm">{legendSettings.description}</span>
+              )}
+            </div>
+          );
+        }
       })}
     </div>
   );
