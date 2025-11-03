eb6d302c73a62e98d1c52bfa38d966516e6cee41
plamendochev <pdochev@cmcl.io>
2025-11-03 16:39:29 +0000

    Fix styling for skeleton loading component to ensure consistent background colors

--- code/src/ui/graphic/table/skeleton/table-skeleton.tsx
+++ code/src/ui/graphic/table/skeleton/table-skeleton.tsx
@@ -17,17 +17,18 @@ export default function TableSkeleton({ rows = 5, columns = 6 }: Readonly<TableS
                     {/* Action cells*/}
                     <th className="border-r border-border border-b p-2 md:p-3 sticky left-0 z-20 bg-background">
                         <div className="flex gap-0.5">
-                            <div className="h-8 w-8 bg-muted animate-pulse rounded" />
-                            <div className="h-8 w-8 bg-muted animate-pulse rounded" />
+                            <div className="h-8 w-8 bg-gray-200 dark:bg-gray-800 animate-pulse rounded" />
+                            <div className="h-8 w-8 bg-gray-200 dark:bg-gray-800 animate-pulse rounded" />
+                            <div className="h-8 w-8 bg-gray-200 dark:bg-gray-800 animate-pulse rounded" />
                         </div>
                     </th>
-                    {/* Data cellss */}
+                    {/* Data cells */}
                     {Array.from({ length: columns }).map((_, colIndex) => (
                         <th
                             key={`skeleton-col-${colIndex}`}
                             className="border-r bg-background border-border border-b p-2 md:p-3 animate-pulse whitespace-nowrap last:border-r-0"
                         >
-                            <div className="h-6 bg-gray-200 animate-pulse rounded w-full max-w-[200px]" />
+                            <div className="h-6 bg-gray-200 dark:bg-gray-800 animate-pulse rounded w-full max-w-[200px]" />
                         </th>
                     ))}
                 </tr>
