df1d98537325ca68804c70452ef20fc4c4c2165b
plamendochev <pdochev@cmcl.io>
2025-11-05 14:10:26 +0000

    Implemented billing table component

---
+++ code/src/ui/graphic/table/billing/billing-table-component.tsx
@@ -0,0 +1,244 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { TableDescriptor, useTable } from "hooks/table/useTable";
+import { useDictionary } from "hooks/useDictionary";
+import useOperationStatus from "hooks/useOperationStatus";
+import { DateRange } from "react-day-picker";
+import { selectDrawerIsOpen } from "state/drawer-component-slice";
+import { AgentResponseBody } from "types/backend-agent";
+import { Dictionary } from "types/dictionary";
+import {
+    LifecycleStage,
+    RegistryFieldValues,
+    RegistryTaskOption,
+} from "types/form";
+import TaskModal from "ui/interaction/modal/task/task-modal";
+import { Status } from "ui/text/status/status";
+import {
+    getAfterDelimiter,
+    getInitialDateFromLifecycleStage,
+    getUTCDate,
+    parseWordsForLabels,
+} from "utils/client-utils";
+import { makeInternalRegistryAPIwithParams } from "utils/internal-api-services";
+import RegistryTable from "../registry/registry-table"
+import TableRibbon from "./ribbon/table-ribbon";
+import TableSkeleton from "../skeleton/table-skeleton";
+
+interface RegistryTableComponentProps {
+    entityType: string;
+    lifecycleStage: LifecycleStage;
+}
+
+/**
+ * This component renders a registry table for the specified entity.
+ *
+ * @param {string} entityType Type of entity for rendering.
+ * @param {LifecycleStage} lifecycleStage The current stage of a contract lifecycle to display.
+ */
+export default function BillingTableComponent(
+    props: Readonly<RegistryTableComponentProps>
+) {
+    const dict: Dictionary = useDictionary();
+    const pathNameEnd: string = getAfterDelimiter(usePathname(), "/");
+    const isTaskModalOpen: boolean = useSelector(selectDrawerIsOpen);
+    const { refreshFlag, triggerRefresh } = useOperationStatus();
+    const [initialInstances, setInitialInstances] = useState<
+        RegistryFieldValues[]
+    >([]);
+    const [currentInstances, setCurrentInstances] = useState<
+        RegistryFieldValues[]
+    >([]);
+    const [task, setTask] = useState<RegistryTaskOption>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const [selectedDate, setSelectedDate] = useState<DateRange>(
+        getInitialDateFromLifecycleStage(props.lifecycleStage)
+    );
+
+    const tableDescriptor: TableDescriptor = useTable(currentInstances);
+
+    // A hook that refetches all data when the dialogs are closed
+    useEffect(() => {
+        const fetchData = async (): Promise<void> => {
+            setIsLoading(true);
+            try {
+                let instances: RegistryFieldValues[] = [];
+                if (props.lifecycleStage === "report") {
+                    if (pathNameEnd === props.entityType) {
+                        // Fetch active contracts
+                        const activeRes = await fetch(
+                            makeInternalRegistryAPIwithParams(
+                                "contracts",
+                                "active",
+                                props.entityType
+                            ),
+                            { cache: "no-store", credentials: "same-origin" }
+                        );
+                        const activeResBody: AgentResponseBody = await activeRes.json();
+                        let activeInstances =
+                            (activeResBody.data.items as RegistryFieldValues[]) ?? [];
+                        activeInstances = activeInstances.map(
+                            (contract: RegistryFieldValues) => ({
+                                status: {
+                                    value: parseWordsForLabels(Status.ACTIVE),
+                                    type: "literal",
+                                    dataType: "http://www.w3.org/2001/XMLSchema#string",
+                                    lang: "",
+                                },
+                                ...contract,
+                            })
+                        );
+
+                        // Fetch archived contracts
+                        const archivedRes = await fetch(
+                            makeInternalRegistryAPIwithParams(
+                                "contracts",
+                                "archive",
+                                props.entityType
+                            ),
+                            { cache: "no-store", credentials: "same-origin" }
+                        );
+                        const archivedResponseBody: AgentResponseBody =
+                            await archivedRes.json();
+                        const archivedInstances: RegistryFieldValues[] =
+                            (archivedResponseBody.data.items as RegistryFieldValues[]) ?? [];
+                        instances = activeInstances.concat(archivedInstances);
+                    } else {
+                        // Fetch service tasks for a specific contract
+                        const res = await fetch(
+                            makeInternalRegistryAPIwithParams(
+                                "tasks",
+                                props.entityType,
+                                pathNameEnd
+                            ),
+                            {
+                                cache: "no-store",
+                                credentials: "same-origin",
+                            }
+                        );
+                        const resBody: AgentResponseBody = await res.json();
+                        instances = (resBody.data?.items as RegistryFieldValues[]) ?? [];
+                    }
+                } else if (props.lifecycleStage == "outstanding") {
+                    const res = await fetch(
+                        makeInternalRegistryAPIwithParams("outstanding", props.entityType),
+                        { cache: "no-store", credentials: "same-origin" }
+                    );
+                    const resBody: AgentResponseBody = await res.json();
+                    instances = (resBody.data?.items as RegistryFieldValues[]) ?? [];
+                } else if (
+                    props.lifecycleStage == "scheduled" ||
+                    props.lifecycleStage == "closed"
+                ) {
+                    const res = await fetch(
+                        makeInternalRegistryAPIwithParams(
+                            props.lifecycleStage == "scheduled" ? "scheduled" : "closed",
+                            props.entityType,
+                            getUTCDate((selectedDate as DateRange).from)
+                                .getTime()
+                                .toString(),
+                            getUTCDate((selectedDate as DateRange).to)
+                                .getTime()
+                                .toString()
+                        ),
+                        {
+                            cache: "no-store",
+                            credentials: "same-origin",
+                        }
+                    );
+                    const resBody: AgentResponseBody = await res.json();
+                    instances = (resBody.data?.items as RegistryFieldValues[]) ?? [];
+                } else if (props.lifecycleStage == "general") {
+                    const res = await fetch(
+                        makeInternalRegistryAPIwithParams(
+                            "instances",
+                            props.entityType,
+                            "true"
+                        ),
+                        { cache: "no-store", credentials: "same-origin" }
+                    );
+                    const resBody: AgentResponseBody = await res.json();
+                    instances = (resBody.data?.items as RegistryFieldValues[]) ?? [];
+                } else {
+                    const res = await fetch(
+                        makeInternalRegistryAPIwithParams(
+                            "contracts",
+                            props.lifecycleStage.toString(),
+                            props.entityType
+                        ),
+                        { cache: "no-store", credentials: "same-origin" }
+                    );
+                    const resBody: AgentResponseBody = await res.json();
+                    instances = (resBody.data?.items as RegistryFieldValues[]) ?? [];
+                }
+                setInitialInstances(instances);
+                setCurrentInstances(instances);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error fetching instances", error);
+            }
+        };
+
+        // Trigger fetchData when refreshFlag, or selectedDate (range) changes
+        fetchData();
+    }, [selectedDate, refreshFlag]);
+
+    useEffect(() => {
+        // Trigger refresh when back navigation occurs
+        const handleHistoryChange = () => {
+            triggerRefresh();
+        };
+        window.addEventListener("popstate", handleHistoryChange);
+        return () => {
+            window.removeEventListener("popstate", handleHistoryChange);
+        };
+    }, []);
+
+    return (
+        <div className="bg-muted mx-auto overflow-auto w-full p-2.5 sm:p-4 md:p-4 h-dvh">
+            <div className="rounded-lg md:p-4 ">
+                <h1 className="text-2xl md:text-4xl font-bold mb-1 sm:mb-4 ">
+                    {parseWordsForLabels(props.entityType)}
+                </h1>
+                <TableRibbon
+                    path={pathNameEnd}
+                    entityType={props.entityType}
+                    selectedDate={selectedDate as DateRange}
+                    setSelectedDate={setSelectedDate}
+                    lifecycleStage={props.lifecycleStage}
+                    instances={initialInstances}
+                    triggerRefresh={triggerRefresh}
+                    tableDescriptor={tableDescriptor}
+                />
+            </div>
+            <div className="flex flex-col overflow-auto gap-y-2 py-4  md:p-4">
+                {refreshFlag || isLoading ? (
+                    <TableSkeleton />
+                ) : currentInstances.length > 0 ? (
+                    <RegistryTable
+                        recordType={props.entityType}
+                        lifecycleStage={props.lifecycleStage}
+                        instances={currentInstances}
+                        setTask={setTask}
+                        tableDescriptor={tableDescriptor}
+                        triggerRefresh={triggerRefresh}
+                    />
+                ) : (
+                    <div className="text-lg ml-6">{dict.message.noResultFound}</div>
+                )}
+            </div>
+            {isTaskModalOpen && task && (
+                <TaskModal
+                    entityType={props.entityType}
+                    task={task}
+                    setTask={setTask}
+                    onSuccess={triggerRefresh}
+                />
+            )}
+        </div>
+    );
+}
---
+++ code/src/ui/graphic/table/billing/ribbon/table-ribbon.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import { usePermissionScheme } from "hooks/auth/usePermissionScheme";
+import { TableDescriptor } from "hooks/table/useTable";
+import { useDictionary } from "hooks/useDictionary";
+import { Routes } from "io/config/routes";
+import React from "react";
+import { DateRange } from "react-day-picker";
+import { PermissionScheme } from "types/auth";
+import { Dictionary } from "types/dictionary";
+import { LifecycleStage, RegistryFieldValues } from "types/form";
+import { DownloadButton } from "ui/interaction/action/download/download";
+import RedirectButton from "ui/interaction/action/redirect/redirect-button";
+import ReturnButton from "ui/interaction/action/redirect/return-button";
+import Button from "ui/interaction/button";
+import DateInput from "ui/interaction/input/date-input";
+import ColumnToggle from "../../action/column-toggle";
+import { getDisabledDates } from "../billing-table-utils";
+
+interface TableRibbonProps {
+    path: string;
+    entityType: string;
+    selectedDate: DateRange;
+    lifecycleStage: LifecycleStage;
+    instances: RegistryFieldValues[];
+    setSelectedDate: React.Dispatch<React.SetStateAction<DateRange>>;
+    triggerRefresh: () => void;
+    tableDescriptor: TableDescriptor;
+}
+
+/**
+ * Renders a ribbon for the view page
+ *
+ * @param {string} path The current path name after the last /.
+ * @param {string} entityType The type of entity.
+ * @param {DateRange} selectedDate The selected date range object with 'from' and 'to' date strings.
+ * @param {LifecycleStage} lifecycleStage The current stage of a contract lifecycle to display.
+ * @param {RegistryFieldValues[]} instances The target instances to export into csv.
+ * @param setSelectedDate A dispatch method to update selected date range.
+ * @param triggerRefresh Method to trigger refresh.
+ * @param {TableDescriptor} tableDescriptor A descriptor containing the required table functionalities and data.
+ */
+export default function TableRibbon(props: Readonly<TableRibbonProps>) {
+    const dict: Dictionary = useDictionary();
+    const keycloakEnabled = process.env.KEYCLOAK === "true";
+    const permissionScheme: PermissionScheme = usePermissionScheme();
+    const triggerRefresh: React.MouseEventHandler<HTMLButtonElement> = () => {
+        props.triggerRefresh();
+    };
+
+    return (
+        <div className="flex flex-col p-1 md:p-2 gap-2 md:gap-4">
+            {props.lifecycleStage !== "general" &&
+                (!keycloakEnabled ||
+                    !permissionScheme ||
+                    permissionScheme.hasPermissions.registry) && (
+                    <div className="bg-ring w-full sm:max-w-fit rounded-lg p-2 sm:p-1.5 border border-border ">
+                        <div className="flex flex-wrap items-center justify-between sm:gap-4 gap-1">
+                            {(!keycloakEnabled ||
+                                permissionScheme?.hasPermissions.pendingRegistry) && (
+                                    <div className="sm:w-auto">
+                                        <RedirectButton
+                                            label="Billing accounts"
+                                            leftIcon="account_balance_wallet"
+                                            hasMobileIcon={false}
+                                            url={Routes.BILLING_BILLING_ACCOUNTS}
+                                            variant={
+                                                props.entityType == "billing_accounts" ? "active" : "ghost"
+                                            }
+                                            className="w-full sm:w-auto py-3 sm:py-2 text-sm font-medium"
+                                        />
+                                    </div>
+                                )}
+                            <div className="sm:w-auto">
+                                <RedirectButton
+                                    label="Pricing models"
+                                    leftIcon="price_change"
+                                    hasMobileIcon={false}
+                                    url={`${Routes.BILLING_PRICING_MODELS}`}
+                                    variant={
+                                        props.entityType == "pricing_models" ? "active" : "ghost"
+                                    }
+                                    className="w-full sm:w-auto py-3 sm:py-2 text-sm font-medium"
+                                />
+                            </div>
+                            <div className="sm:w-auto">
+                                <RedirectButton
+                                    label="Billing activity"
+                                    leftIcon="receipt_long"
+                                    hasMobileIcon={false}
+                                    url={`${Routes.BILLING_BILLING_ACTIVITY}`}
+                                    variant={
+                                        props.entityType == "billing_activity" ? "active" : "ghost"
+                                    }
+                                    className="w-full sm:w-auto py-3 sm:py-2 text-sm font-medium"
+                                />
+                            </div>
+
+                        </div>
+                    </div>
+                )}
+            <div className="w-full  h-[1px] bg-border " />
+            <div className="flex justify-between items-end md:gap-2 lg:gap-0 flex-wrap ">
+                <div className="flex items-end !-ml-2 gap-3 md:gap-4">
+                    <Button
+                        className="ml-2"
+                        size="icon"
+                        leftIcon="cached"
+                        variant="outline"
+                        onClick={triggerRefresh}
+                    />
+                    {(props.lifecycleStage == "scheduled" ||
+                        props.lifecycleStage == "closed") && (
+                            <DateInput
+                                selectedDate={props.selectedDate}
+                                setSelectedDateRange={props.setSelectedDate}
+                                disabledDates={getDisabledDates(props.lifecycleStage)}
+                            />
+                        )}
+                </div>
+                <div className="flex items-end flex-wrap gap-2 mt-2 md:mt-0  ">
+                    {props.tableDescriptor.table
+                        .getState()
+                        .columnFilters?.some(
+                            (filter) => (filter?.value as string[])?.length > 0
+                        ) && (
+                            <Button
+                                leftIcon="filter_list_off"
+                                iconSize="medium"
+                                className="mt-1"
+                                size="icon"
+                                onClick={() => {
+                                    props.tableDescriptor.table.resetColumnFilters()
+                                    props.tableDescriptor.table.resetRowSelection()
+                                }}
+                                tooltipText={dict.action.clearAllFilters}
+                                variant="destructive"
+                            />
+                        )}
+                    {props.instances.length > 0 && (
+                        <ColumnToggle
+                            columns={props.tableDescriptor.table.getAllLeafColumns()}
+                        />
+                    )}
+                    {(!keycloakEnabled ||
+                        !permissionScheme ||
+                        permissionScheme.hasPermissions.sales) &&
+                        (props.lifecycleStage == "pending" ||
+                            props.lifecycleStage == "general") && (
+                            <RedirectButton
+                                leftIcon="add"
+                                size="icon"
+                                tooltipText={dict.action.addItem.replace(
+                                    "{replace}",
+                                    props.entityType.replace("_", " ")
+                                )}
+                                url={`${Routes.REGISTRY_ADD}/${props.entityType}`}
+                            />
+                        )}
+                    {props.lifecycleStage == "report" && (
+                        <ReturnButton
+                            leftIcon="first_page"
+                            label={dict.action.backTo.replace(
+                                "{replace}",
+                                props.entityType.replace("_", " ")
+                            )}
+                        />
+                    )}
+                    {(!keycloakEnabled ||
+                        !permissionScheme ||
+                        permissionScheme.hasPermissions.invoice) &&
+                        props.lifecycleStage == "report" && (
+                            <RedirectButton
+                                leftIcon="print"
+                                label={dict.action.generateReport}
+                                url={`${Routes.REGISTRY_EDIT}/pricing/${props.path}`}
+                            />
+                        )}
+                    {(!keycloakEnabled ||
+                        !permissionScheme ||
+                        permissionScheme.hasPermissions.export) && (
+                            <DownloadButton instances={props.instances} />
+                        )}
+                </div>
+            </div>
+        </div>
+    );
+}
