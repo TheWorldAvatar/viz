23298cf13aead47457ddd885e33a242e725712e4
PDochev <plamendochev0@gmail.com>
2025-11-06 10:17:57 +0000

    Update NotFound component header size for improved visibility

--- code/src/app/[lang]/not-found.tsx
+++ code/src/app/[lang]/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
     const router = useRouter();
     return (
         <div className="fixed z-50 inset-0 left-0 top-0 bg-gradient-to-b from-background via-muted to-primary flex min-h-screen items-center justify-center flex-col space-y-4">
-            <h1 className="scroll-m-20 text-6xl md:text-8xl font-extrabold tracking-tight lg:text-[13rem] mt-8 mb-8 text-destructive underline">
+            <h1 className="scroll-m-20 text-6xl md:text-8xl font-extrabold tracking-tight lg:text-9xl mt-8 mb-8 text-destructive underline">
                 404
             </h1>
             <p className="text-foreground">{dict.message.notFound}</p>
