d771ba61197371d7cb4e976e7705bdd8acac0fa2
PDochev <plamendochev0@gmail.com>
2025-11-07 09:26:32 +0000

    Update text color in TextField component for improved readability

--- code/src/ui/text/field/field.tsx
+++ code/src/ui/text/field/field.tsx
@@ -15,7 +15,7 @@ export default function TextField(props: Readonly<TextFieldProps>) {
       <h4 className="flex-shrink-0 w-28 sm:w-32 text-sm sm:text-base font-semibold text-foreground capitalize">
         {props.label}
       </h4>
-      <p className="flex-1 min-w-0 text-wrap hyphens-auto break-all text-sm  text-gray-500 dark:text-gray-300">
+      <p className="flex-1 min-w-0 text-wrap hyphens-auto break-all text-sm  text-gray-600 dark:text-gray-300">
         {props.content}
       </p>
     </div>
