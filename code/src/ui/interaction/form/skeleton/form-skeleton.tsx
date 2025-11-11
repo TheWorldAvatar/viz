766f73658694fd759c03aeab7f7030979766da82
plamendochev <pdochev@cmcl.io>
2025-11-11 15:47:18 +0000

    Refactor FormSkeleton component for improved structure and consistency

--- code/src/ui/interaction/form/skeleton/form-skeleton.tsx
+++ code/src/ui/interaction/form/skeleton/form-skeleton.tsx
@@ -1,18 +1,18 @@
 export default function FormSkeleton() {
     return (
-        <div className="w-full h-full flex flex-col overflow-hidden">
-            <div className="flex-1 p-4 animate-pulse space-y-4 overflow-auto">
+        <div className="w-full h-full flex flex-col overflow-hidden ">
+            <div className="flex-1 p-4 animate-pulse space-y-4 overflow-hidden">
 
                 <div className="space-y-4">
-                    {[...Array(8)].map((_, index) => (
+                    {[...Array(4)].map((_, index) => (
                         <div key={`skeleton-field-${index}`} className="space-y-2">
-                            <div className="h-12 bg-ring rounded w-full" />
-                            <div className="h-12 bg-ring rounded w-full" />
+                            <div className="h-12 md:h-12 bg-ring rounded w-full" />
                             <div className="h-26 bg-ring rounded w-full" />
                             <div className="flex items-center justify-between">
                                 <div className="h-12 bg-ring rounded w-46 max-w-24" />
                                 <div className="h-12 bg-ring rounded w-46" />
                             </div>
+
                         </div>
                     ))}
                 </div>
