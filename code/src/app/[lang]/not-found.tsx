7a702319fa042982accaebafdd75982827778a69
plamendochev <pdochev@cmcl.io>
2025-11-12 09:45:41 +0000

    Insert an 100vh (svh) for the not found page

--- code/src/app/[lang]/not-found.tsx
+++ code/src/app/[lang]/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     const dict: Dictionary = useDictionary();
     const router = useRouter();
     return (
-        <div className="flex items-center justify-center flex-col space-y-4">
+        <div className="flex items-center justify-center flex-col space-y-4 h-svh">
             <h1 className="scroll-m-20 text-6xl md:text-8xl font-extrabold tracking-tight mt-20 mb-8 text-destructive underline">
                 404
             </h1>
