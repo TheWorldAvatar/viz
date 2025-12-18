fd0449f588ef7c57213174e912888ae001182b6f
plamendochev <pdochev@cmcl.io>
2025-12-18 13:53:39 +0000

    Add loading spinner and enhance cost details display in BillingModal

--- code/src/ui/interaction/modal/billing-modal.tsx
+++ code/src/ui/interaction/modal/billing-modal.tsx
@@ -6,6 +6,8 @@ import { Dictionary } from "types/dictionary";
 import { FormTypeMap } from "types/form";
 import { makeInternalRegistryAPIwithParams, queryInternalApi } from "utils/internal-api-services";
 import Modal from "./modal";
+import LoadingSpinner from "ui/graphic/loader/spinner";
+
 
 interface BillingModalProps {
     id: string;
@@ -25,9 +27,11 @@ interface BillingModalProps {
 export default function BillingModal(props: Readonly<BillingModalProps>) {
     const dict: Dictionary = useDictionary();
     const [costDetails, setCostDetails] = useState<ServiceCostDetails>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     // A hook that fetches data initially
     useEffect(() => {
         const fetchData = async (): Promise<void> => {
+            setIsLoading(true);
             const url: string = makeInternalRegistryAPIwithParams(
                 InternalApiIdentifierMap.BILL,
                 FormTypeMap.VIEW,
@@ -36,52 +40,60 @@ export default function BillingModal(props: Readonly<BillingModalProps>) {
 
             const res: AgentResponseBody = await queryInternalApi(url);
             setCostDetails(res.data.items[0] as ServiceCostDetails);
+            setIsLoading(false);
         };
 
         fetchData();
     }, []);
 
     return (
-        <Modal className="!w-full !max-w-3xl !h-auto !min-h-0 !rounded-xl !shadow-xl !border !border-border" isOpen={props.isOpen} setIsOpen={props.setIsOpen}>
-            <div className="flex flex-col p-2  w-full max-w-2xl mx-auto">
+        <Modal className="!w-full  md:!max-w-2xl lg:!max-w-3xl  !h-auto !min-h-0 !rounded-xl !shadow-xl !border !border-border" isOpen={props.isOpen} setIsOpen={props.setIsOpen}>
+            <div className="flex flex-col p-2  w-full max-w-2xl mx-auto mt-4">
                 <div className="flex  items-center justify-between">
-                    <h2 className="text-xl font-semibold ">{dict.title.serviceCostBreakdown}</h2>
-                    <p>{props.date}</p>
+                    <h2 className="text-xl font-semibold text-foreground">{dict.title.serviceCostBreakdown}</h2>
+                    <p className="text-foreground">{props.date}</p>
                 </div>
                 <div>
-                    <div className="flex  items-center justify-between mt-4 bg-ring p-2 rounded-sm">
-                        <p className="font-bold ">{dict.title.item}</p>
+                    <div className="flex items-center justify-between mt-4 bg-ring p-2.5 rounded-sm">
+                        <p className="font-bold text-foreground">{dict.title.item}</p>
                         <p className="text-muted-foreground">{dict.title.amount}</p>
                     </div>
-                    {costDetails != null && <>
-                        <div>
-                            {/* Base service cost */}
-                            <div className="flex p-2 justify-between mt-2">
-                                <p className="text-md max-w-sm font-bold">{dict.title.serviceCharge}</p>
-                                <p className="text-muted-foreground">${costDetails?.amount}</p>
+                    {costDetails != null && !isLoading ? (
+                        <div className="flex flex-col max-h-[60vh]">
+                            <div className="flex-1 overflow-y-auto pr-2">
+                                {/* Base service cost */}
+                                <div className="flex p-2 justify-between mt-2">
+                                    <p className="text-md max-w-sm font-bold">{dict.title.serviceCharge}</p>
+                                    <p className="text-muted-foreground">${costDetails?.amount}</p>
+                                </div>
+
+                                {/* Additional charges */}
+                                {costDetails?.charge?.map((charge, index) => (
+                                    <div key={index} className="flex p-2 justify-between">
+                                        <p className="text-sm w-58 md:w-md text-gray-600 dark:text-gray-300">{charge?.description}</p>
+                                        <p className="text-green-600 dark:text-green-700">+ ${charge?.amount}</p>
+                                    </div>
+                                ))}
+
+                                {/* Discounts */}
+                                {costDetails?.discount?.map((discount, index) => (
+                                    <div key={index} className="flex p-2 justify-between">
+                                        <p className="text-sm w-58 md:w-md text-gray-600 dark:text-gray-300">{discount?.description}</p>
+                                        <p className="text-red-600 dark:text-red-700">- ${discount?.amount}</p>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <div className="flex items-center justify-end gap-10 mt-2 p-2 border-t border-border">
+                                <p>{dict.title.total}</p>
+                                <p className="font-bold">${costDetails?.price}</p>
                             </div>
                         </div>
-                        <div>
-                            {/* Additional charges */}
-                            {costDetails?.charge?.map((charge, index) => (
-                                <div key={index} className="flex  p-2 justify-between">
-                                    <p className="text-sm max-w-sm text-gray-500 dark:text-gray-300">{charge?.description}</p>
-                                    <p className="text-muted-foreground">+ ${charge?.amount}</p>
-                                </div>
-                            ))}
-                            {/* Discounts */}
-                            {costDetails?.discount?.map((discount, index) => (
-                                <div key={index} className="flex  p-2  justify-between">
-                                    <p className="text-sm max-w-sm text-gray-500 dark:text-gray-300">{discount?.description}</p>
-                                    <p className="text-muted-foreground">- ${discount?.amount}</p>
-                                </div>
-                            ))}
+                    ) : (
+                        <div className="flex justify-center items-center h-48">
+                            <LoadingSpinner isSmall={false} />
                         </div>
-                        <div className="flex items-center justify-end gap-10 mt-2 p-2 border-b border-t border-border">
-                            <p>{dict.title.total}</p>
-                            <p className="font-bold">${costDetails?.price}</p>
-                        </div>
-                    </>}
+                    )}
                 </div>
             </div>
         </Modal>
