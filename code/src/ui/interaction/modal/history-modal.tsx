1ed12f1de9787cc59d3bab0befa6e42e494c7b5b
plamendochev <pdochev@cmcl.io>
2026-01-19 13:27:51 +0000

    added new dictionary translations and changed the design of the history modal

--- code/src/ui/interaction/modal/history-modal.tsx
+++ code/src/ui/interaction/modal/history-modal.tsx
@@ -2,7 +2,6 @@ import { useDictionary } from "hooks/useDictionary";
 import { useEffect, useState } from "react";
 import { AgentResponseBody, HistoryDetails, InternalApiIdentifierMap } from "types/backend-agent";
 import { Dictionary } from "types/dictionary";
-import { LifecycleStage, LifecycleStageMap } from "types/form";
 import LoadingSpinner from "ui/graphic/loader/spinner";
 import { formatValueByDataType } from "ui/graphic/table/registry/registry-table-utils";
 import { XSD_DATETIME } from "utils/constants";
@@ -12,7 +11,6 @@ import Modal from "./modal";
 interface HistoryModalProps {
     id: string;
     entityType: string;
-    lifecycleStage: LifecycleStage;
     isOpen: boolean;
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -22,7 +20,6 @@ interface HistoryModalProps {
  *
  * @param {string} id - The identifier for the entity.
  * @param {string} entityType - The type of entity being viewed.
- * @param {LifecycleStage} lifecycleStage The current stage of a contract lifecycle to display.
  * @param {boolean} isOpen Indicates if modal should be initially open.
  * @param  setIsOpen Sets the isOpen parameter.
  */
@@ -38,11 +35,7 @@ export default function HistoryModal(props: Readonly<HistoryModalProps>) {
             const url: string = makeInternalRegistryAPIwithParams(
                 InternalApiIdentifierMap.HISTORY,
                 props.id,
-                props.lifecycleStage == LifecycleStageMap.ACTIVITY ? "bill" :
-                    props.lifecycleStage == LifecycleStageMap.OUTSTANDING ||
-                        props.lifecycleStage == LifecycleStageMap.SCHEDULED ||
-                        props.lifecycleStage == LifecycleStageMap.CLOSED ? "task" :
-                        props.entityType,
+                props.entityType,
             );
             const res: AgentResponseBody = await queryInternalApi(url);
             setHistoryDetails(res.data.items as HistoryDetails[]);
@@ -53,23 +46,38 @@ export default function HistoryModal(props: Readonly<HistoryModalProps>) {
     }, []);
 
     return (
-        <Modal className="!w-full  md:!max-w-2xl lg:!max-w-3xl  !h-auto !min-h-0 !rounded-xl !shadow-xl !border !border-border" isOpen={props.isOpen} setIsOpen={props.setIsOpen}>
-            <div className="flex flex-col p-2  w-full max-w-2xl mx-auto mt-4">
-                <h2 className="text-xl font-semibold text-foreground">{dict.title.history}</h2>
-                {!isLoading && <table>
-                    <tr className="bg-ring rounded-sm">
-                        <th className="text-foreground">{dict.action.date}</th>
-                        <th className="text-foreground">{dict.title.user}</th>
-                        <th className="text-foreground">{dict.title.actions}</th>
-                    </tr>
-                    {historyDetails.map((history, index) => (
-                        <tr key={index} className="text-gray-600 dark:text-gray-300">
-                            <td >{formatValueByDataType(history?.timestamp?.value, XSD_DATETIME)}</td>
-                            <td >{history?.user?.value ?? "-"}</td>
-                            <td>{history?.message?.value}</td>
-                        </tr>
-                    ))}
-                </table>}
+        <Modal className="!w-full md:!max-w-2xl lg:!max-w-4xl !h-auto  !min-h-[60vh] max-h-[60vh] md:!min-h-[70vh] md:!max-h-[70vh] !rounded-xl !shadow-xl !border !border-border p-4" isOpen={props.isOpen} setIsOpen={props.setIsOpen}>
+            <div className="flex flex-col my-auto w-full mt-10">
+                <div className="flex justify-between items-center mb-4">
+                    <div>
+                        <h1 className="text-base md:text-lg font-semibold">{dict.title.history}</h1>
+                        <h2 className="text-sm md:text-base text-foreground">{dict.message.modifications}</h2>
+                    </div>
+                    <p className="text-sm md:text-base">{historyDetails.length} {historyDetails.length > 1 || historyDetails.length === 0 ? dict.message.entries : dict.message.entry}</p>
+                </div>
+                {!isLoading && (
+
+                    <div className="mt-2 w-full overflow-y-auto max-h-[37vh] md:max-h-[55vh] border rounded-lg border-border/50">
+                        <table className="w-full table-auto border-collapse">
+                            <thead>
+                                <tr className="text-left text-sm text-foreground">
+                                    <th className="sticky top-0 bg-ring z-10 border-b border-border p-2">{dict.action.date}</th>
+                                    <th className="sticky top-0 bg-ring z-10 border-b border-border p-2">{dict.title.user}</th>
+                                    <th className="sticky top-0 bg-ring z-10 border-b border-border p-2">{dict.title.actions}</th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-border/50">
+                                {historyDetails.map((history, index) => (
+                                    <tr key={index} className="text-sm text-muted-foreground hover:bg-muted/30 ">
+                                        <td className="py-3 px-2">{formatValueByDataType(history?.timestamp?.value, XSD_DATETIME)}</td>
+                                        <td className="py-3 px-2">{history?.user?.value ?? "-"}</td>
+                                        <td className="py-3 px-2">{history?.message?.value}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
                 {isLoading && <div className="flex justify-center items-center h-48">
                     <LoadingSpinner isSmall={false} />
                 </div>}
