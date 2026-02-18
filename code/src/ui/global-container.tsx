ea1653d264edb1033de11fe5ee76ab32cacde2ff
qhouyee <quekhouyee@gmail.com>
2026-02-18 16:05:03 +0000

    refactor contextmenu into an useeffect

--- code/src/ui/global-container.tsx
+++ code/src/ui/global-container.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Konami from "react-konami-code";
 import { Provider } from "react-redux";
 
 import { reduxStore } from "app/store";
 import { useBackgroundImageUrl } from "hooks/useBackgroundImageUrl";
 import { OptionalPage } from "io/config/optional-pages";
+import { usePathname } from "next/navigation";
 import { UISettings } from "types/settings";
 import Trex from "utils/trex";
 import ContextMenu from "./interaction/context-menu/context-menu";
 import HeaderBar from "./interaction/header/headerbar";
-import Footer from "./text/footer";
 import { NavMenu } from "./navigation/navbar/nav-menu";
-import { usePathname } from "next/navigation";
+import Footer from "./text/footer";
 
 // Incoming properties for global container
 interface GlobalContainerProps {
@@ -39,25 +39,23 @@ export default function GlobalContainer(props: Readonly<GlobalContainerProps>) {
     setPopup(!popup);
   };
 
-  // Method to handle right-click and show the context menu
-  const handleContextMenu = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setContextMenuVisible(true);
-    setContextMenuPosition({ x: e.pageX, y: e.pageY });
-  };
-
-  // Method to close the context menu when it is no longer needed
-  const closeContextMenu = () => {
-    setContextMenuVisible(false);
-  };
+  useEffect(() => {
+    const handleContextMenu = (e: MouseEvent) => {
+      e.preventDefault();
+      setContextMenuVisible(true);
+      setContextMenuPosition({ x: e.pageX, y: e.pageY });
+    };
+    window.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      window.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
 
   return (
     <Provider store={reduxStore}>
       <HeaderBar pages={props.pages} settings={props.settings} />
 
       <main className="flex h-[92dvh] w-full"
-        onContextMenu={handleContextMenu}
-        onClick={closeContextMenu} // Close context menu when clicking elsewhere
         style={{
           backgroundImage: `url(${backgroundImageUrl})`,
           backgroundSize: "cover",
