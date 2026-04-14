32820f58ea25b2ebba31df93a33a2d58c1bed3ff
qhouyee <quekhouyee@gmail.com>
2026-04-14 15:40:21 +0000

    allow users to bulk assign the same details at a global level

--- code/src/utils/constants.ts
+++ code/src/utils/constants.ts
@@ -2,6 +2,7 @@ export const XSD_DATETIME = "http://www.w3.org/2001/XMLSchema#dateTime";
 export const XSD_DATE = "http://www.w3.org/2001/XMLSchema#date";
 export const XSD_TIME = "http://www.w3.org/2001/XMLSchema#time";
 
+export const BULK_IDENTIFIER= "bulk";
 export const DATE_KEY = "date";
 export const EVENT_KEY = "event";
 export const REPLACE_DICT_KEY = "{replace}";
