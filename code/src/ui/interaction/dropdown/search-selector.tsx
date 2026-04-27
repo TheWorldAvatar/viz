2e1bbb5977c8a128dbc79c82f15571041682c0fa
qhouyee <quekhouyee@gmail.com>
2026-04-27 10:30:03 +0000

    fix untranslated status filter

--- code/src/ui/interaction/dropdown/search-selector.tsx
+++ code/src/ui/interaction/dropdown/search-selector.tsx
@@ -93,7 +93,7 @@ export default function SearchSelector(props: Readonly<SearchSelectorProps>) {
           <SelectOption
             key={option + index}
             option={props.label === dict.title.status ? dict.title[option.toLowerCase()] : option}
-            labelComponent={props.label === dict.title.status ? <StatusComponent status={option} /> : null}
+            labelComponent={props.label === "status" ? <StatusComponent status={option} /> : null}
             initialChecked={selectedOptions.includes(option)}
             onClick={() => {
               if (selectedOptions.includes(option)) {
