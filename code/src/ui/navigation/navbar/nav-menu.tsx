f57d13ecd13028997af43b5d1881bcd08c7e83d9
plamendochev <pdochev@cmcl.io>
2026-05-19 16:44:58 +0100

    Update NavMenuContents styling for mobile responsiveness and improve overflow handling

--- code/src/ui/navigation/navbar/nav-menu.tsx
+++ code/src/ui/navigation/navbar/nav-menu.tsx
@@ -148,7 +148,7 @@ function NavMenuContents(
       aria-label={dict.nav.title.primary}
       ref={navMenuRef}
       className={`${props.isMobile
-        ? "flex gap-4 p-2 w-full"
+        ? "flex gap-4 p-2 w-full max-h-[60dvh] overflow-y-auto"
         : "items-center gap-4 overflow-x-hidden px-0 xl:px-4 pb-4 shrink-0"
         }
       xl:flex flex-col ${props.isMenuExpanded ? "items-stretch" : "items-center"
@@ -174,7 +174,6 @@ function NavMenuContents(
         />
       )}
       {props.settings?.modules?.landing && (
-
         <NavBarItem
           title={dict.nav.title.home}
           icon="home"
