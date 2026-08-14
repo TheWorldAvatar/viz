7d95ca21be53e8df615db20f6cdcc379c8575dbe
plamendochev <pdochev@cmcl.io>
2026-08-14 14:15:44 +0100

    PDochev/862/extend-ui-settings-for-export-agent Update parameter documentation in queryDefaultFileExportAPI to specify that 'id' is a single string instead of an array.

--- code/utils/internal-api-services.ts
+++ code/utils/internal-api-services.ts
@@ -292,7 +292,7 @@ export async function queryDefaultFileExportAPI(id: string, selectedDate: DateRa
 /**
  * Queries the file export API to retrieve the file download object and name.
  * 
- * @param {string | string[]} id The target ID(s) of the resource.
+ * @param {string} id The target ID of the resource.
  * @param {string} resource The resource type.
  * @param {"csv" | "pdf"} format The file format (csv or pdf).
  */
