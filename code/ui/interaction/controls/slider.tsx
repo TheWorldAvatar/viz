7fd9e7246d32d90a872fdcdb8487cbe370c5edd7
plamendochev <pdochev@cmcl.io>
2026-08-27 09:42:45 +0100

    PDochev/874/replace-mui-components Removed Slider MUI component and replaced with native range html input

--- code/ui/interaction/controls/slider.tsx
+++ code/ui/interaction/controls/slider.tsx
@@ -1,67 +1,54 @@
-import Slider from '@mui/material/Slider';
 import * as React from 'react';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectDimensionSliderValue, setValue } from '@/state/dimension-slider-slice';
 import { ScenarioDimensionsData, ScenarioDimensionStep } from '@/types/timeseries';
-import styles from './slider.module.css';
 
 interface DimensionSliderProps {
     data: ScenarioDimensionsData;
 }
 
 function valuetext(value: number, values: ScenarioDimensionStep[]) {
-    const selectedStep = values.find(step => step.value === value);
+    const selectedStep: ScenarioDimensionStep = values.find(step => step.value === value);
     return selectedStep ? selectedStep.label : 'Unknown';
 }
 
 
 export default function DimensionSlider({ data }: DimensionSliderProps) {
-    const values = Object.values(data).flat();
-    const min = values[0]?.value;
-    const max = values[values.length - 1]?.value;
-    const middle = Math.round((min + max) / 2);
+    const values: ScenarioDimensionStep[] = Object.values(data).flat();
+    const min: number = values[0]?.value;
+    const max: number = values[values.length - 1]?.value;
     const dispatch = useDispatch();
-    const dimensionSliderValue = useSelector(selectDimensionSliderValue);
+    const dimensionSliderValue: number | number[] = useSelector(selectDimensionSliderValue);
     const [tempValue, setTempValue] = useState(dimensionSliderValue);
 
-    const handleChange = (event: React.SyntheticEvent | Event, newValue: number | number[]) => {
-        setTempValue(newValue);
+    const currentValue: number = Array.isArray(tempValue) ? tempValue[0] : tempValue;
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTempValue(Number(event.target.value));
     };
 
-    const handleChangeCommitted = (
-        event: React.SyntheticEvent | Event,
-        value: number | number[],
-        values: ScenarioDimensionStep[]
-    ) => {
-        // Slider supports multiple thumbs, so 'value' could be an array, need to handle both cases
-        const firstValue = Array.isArray(value) ? value[0] : value;
-        const selectedStep = values.find(step => step.value === firstValue);
-        // label is used
-
-        const label = selectedStep ? selectedStep.label : 'Unknown';
-
-        dispatch(setValue(value));
+    const handleChangeCommitted = () => {
+        dispatch(setValue(currentValue));
     }
 
     return (
-        <div className={styles.sliderContainer}>
+        <div className="flex flex-col  justify-center w-full h-full p-2">
             {/* <span>{Object.keys(data).flat()}</span> ---TODO slider label if we want later */}
-            <Slider
+            <output>{valuetext(currentValue, values)}</output>
+            <input
                 key={`slider-${Object.keys(data).flat()}`}
-                className={styles.discreteSlider}
+                className="w-full"
+                type="range"
                 aria-label="Time"
-                defaultValue={middle}
-                value={tempValue}
-                valueLabelFormat={(value: number) => valuetext(value, values)}
-                valueLabelDisplay="auto"
-                shiftStep={1}
+                aria-valuetext={valuetext(currentValue, values)}
+                value={currentValue}
                 step={1}
-                marks
                 min={min}
                 max={max}
                 onChange={handleChange}
-                onChangeCommitted={(event: React.SyntheticEvent | Event, value: number | number[]) => handleChangeCommitted(event, value, values)}
+                onPointerUp={handleChangeCommitted}
+                onKeyUp={handleChangeCommitted}
             />
         </div>
     );
